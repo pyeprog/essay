ee8d39791025501f8575d411fce1c197e452abe4
pleon <pyeprog@foxmail.com>
2016-04-01 23:53:33 +0800

    DivRstSet +1, div_search +1

--- div_search.py
+++ div_search.py
@@ -5,63 +5,28 @@
 # The diversity search result - divRet = {1: ((id, score)), 2: ((..))}
 # The k-th largest score of any time - kthLrgScore = int
 
-import tool.Graph as grph
+import tool.Graph as graph
+import tool.DivRstSet as div
 
 
 def div_search(k, tuples, eTable, div_func):
     assert(k > 0)
     if k >= len(tuples):
-        divRet = dict()
-        divRet[len(tuples)] = tuple(tuples)
-        return divRet
+        D = div.DivRstSet(tuples)
+        return D
     tuples.sort(key=lambda x: x[1])
     end = 1
     countAddItem = 0
-    divRet = {1: tuple(tuples[:end])}
+    D = div.DivRstSet(tuples[:end])
     kthLrgScore = tuples[k - 1][1]
-    while maxScore(divRet, len(divRet)) < \
-            bestPsbBound(divRet, k, tuples[end][1]):
+    while D.maxScrAll() < D.upbound(k, tuples[end][1]):
         end += 1
         countAddItem += 1
-        while end == len(tuples) or (countAddItem >= k - max(divRet.keys()) and
+        while end == len(tuples) or (countAddItem >= k - max(D.getKList()) and
                                      kthLrgScore > tuples[end][1]):
             countAddItem = 0
-            divRet = div_func(graph=grph.Graph(tuples[:end], eTable), k=k)
+            divRet = div_func(graph=graph.Graph(tuples[:end], eTable), k=k)
     return divRet
-
-
-def maxScore(divRet, k):
-    """return the max value among those sets whose size <= k"""
-    assert(isinstance(divRet, dict) and isinstance(k, int))
-    ret = 0
-    for key, valList in divRet.iteritems():
-        if key <= k:
-            if not isinstance(valList, tuple):
-                raise TypeError
-            if key != len(valList):
-                raise ValueError
-            ret = max(ret, sum([val[1] for val in valList]))
-    return ret
-
-
-def divRetGetScore(divRet, key):
-    assert(key in divRet)
-    return sum([val[1] for val in divRet[key]])
-
-
-def bestPsbBound(divRet, k, unseenBound):
-    if not (isinstance(divRet, dict) and isinstance(k, int) and
-            (isinstance(unseenBound, int) or isinstance(unseenBound, float))):
-        raise TypeError
-    ret = k * unseenBound
-    for key, valList in divRet.iteritems():
-        assert(k >= key)
-        ret = max(ret, divRetGetScore(divRet, key) + (k - key) * unseenBound)
-    return ret
-
-
-def div_search_cur(**kwarg):
-    print kwarg
 
 
 if __name__ == '__main__':
@@ -72,11 +37,6 @@ if __name__ == '__main__':
         4: ((1, 0.5), (2, 0.2), (3, 0.8), (4, 0.1)),
         5: ((3, 0.8), (1, 0.5), (5, 1), (6, 1), (7, 2))
     }
-    print maxScore(testDivRet, 5)
-    print divRetGetScore(testDivRet, 5)
-    print divRetGetScore(testDivRet, 3)
-    print divRetGetScore(testDivRet, 2)
-    print bestPsbBound(testDivRet, 6, 0.6)
     testVertice = list()
     import random
     for i in xrange(7):
@@ -88,4 +48,4 @@ if __name__ == '__main__':
               (2, 3, 5),
               (0, 1, 4, 6, 7),
               (0, 5))
-    div_search(3, testVertice, eTable, div_search_cur)
+    # div_search(3, testVertice, eTable, div_search_cur)
--- tool/DivRstSet.py
+++ tool/DivRstSet.py
@@ -5,10 +5,26 @@ class DivRstSet(object):
     def __init__(self, iterable, func):
         assert(hasattr(iterable, '__iter__'))
         self._dict = dict()
-        for i in iterable:
-            k, tuples = func(i, iterable)
+        for k, tuples in func(iterable):
             assert(k not in self._dict)
             self._dict[k] = tuples
+
+    @classmethod
+    def new(cls, *args):
+        divSet = DivRstSet({}, DivRstSet._dictTrans)
+        for each in args:
+            divSet.setSet(each)
+        return divSet
+
+    @classmethod
+    def _dictTrans(cls, dictionary):
+        assert(isinstance(dictionary, dict))
+        return dictionary.iteritems()
+
+    @classmethod
+    def _tuplesTrans(cls, tuples):
+        assert(isinstance(tuples, (list, tuple, set)))
+        return tuple([(len(tuples), tuple(tuples))])
 
     def maxScr(self, mink, maxk):
         assert(mink <= maxk)
@@ -20,11 +36,17 @@ class DivRstSet(object):
                 maxScore = max(maxScore, curSum)
         return maxScore
 
-    def setSet(self, k, tuples):
-        assert(isinstance(k, int) and isinstance(tuples, tuple))
-        if k != len(tuples):
-            raise ValueError
-        self._dict[k] = tuples
+    def maxScrAll(self):
+        kList = self._dict.keys()
+        return self.maxScr(min(kList), max(kList))
+
+    def setSet(self, iterable):
+        if isinstance(iterable, (list, tuple, set)):
+            self._dict[len(iterable)] = tuple(iterable)
+        elif isinstance(iterable, dict):
+            for k, t in iterable.iteritems():
+                self[k] = t
+        return self
 
     def rm(self, k):
         self._dict.pop(k)
@@ -53,11 +75,15 @@ class DivRstSet(object):
             bstPsbleVal = max(bstPsbleVal, self.getScr(ck) + (k - ck) * usBst)
         return bstPsbleVal
 
+    def getKList(self):
+        return self._dict.keys()
+
     def __getitem__(self, k):
         return self._dict[k]
 
     def __setitem__(self, k, tuples):
-        self.setSet(k, tuples)
+        assert(k == len(tuples))
+        self.setSet(tuples)
 
     def __str__(self):
         outputStr = ""
@@ -67,18 +93,28 @@ class DivRstSet(object):
 
 
 if __name__ == '__main__':
-    testDict = {1: ((1, 3),), 2: ((1, 3), (4, 4)), 3:((1, 3), (4, 4), (5, 1))}
-
-    def proc(i, Dict):
-        return i, Dict[i]
-
-    test = DivRstSet(testDict, proc)
-    test.setSet(4, ((2, 42), (1, 3), (4, 4), (5, 1)))
+    testDict = {1: ((1, 3),), 2: ((1, 3), (4, 4)), 3: ((1, 3), (4, 4), (5, 1))}
+    test = DivRstSet(testDict, DivRstSet._dictTrans)
+    test.setSet(((2, 42), (1, 3), (4, 4), (5, 1)))
     test.rm(4)
     print test.getSet(3)
     print test.getScr(3)
     print test.hasK(5)
     print test.maxScr(1, 3)
+    print test.maxScrAll()
     print test.upbound(10, 1)
     test[1] = ((3, 2),)
     print test[1]
+    testTuples = [(1, 3), (2, 4), (3, 53)]
+    testTuples1 = set([(1, 3), (2, 4), (3, 53)])
+    testTuples2 = ((1, 3), (2, 4), (3, 53))
+    print DivRstSet(testTuples, DivRstSet._tuplesTrans)
+    print DivRstSet(testTuples1, DivRstSet._tuplesTrans)
+    print DivRstSet(testTuples2, DivRstSet._tuplesTrans)
+    test = DivRstSet.new(testDict)
+    print test
+    test = DivRstSet.new()
+    print test
+    test = DivRstSet.new(testDict, testTuples1)
+    print test
+    print test.getKList()
